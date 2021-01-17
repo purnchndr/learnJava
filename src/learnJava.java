a4fcdc10e2718ae674b578e4111f672c47a9e3fa
Purnachandra <purnchndr7@gmail.com>
2021-01-17 11:26:55 +0530

    Adding a program to shift an array by d element.

--- src/learnJava.java
+++ src/learnJava.java
@@ -1,12 +1,16 @@
-import java.util.ArrayList;
-//import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
-public  class learnJava  {
+class leanJava{
+
+
     public static void main(String[] args) {
-List<Object> list = new ArrayList<>();
-//List<Object> list2 = new LinkedList<>();
-list.add("hello");
-        System.out.println(list.get(0));
+        Map<Integer,Object> list = new HashMap<>();
+        list.put(1,"purna");
+        list.put(2,55);
+        list.put(3,'h');
+        list.put(1 , "Chand");
+        for (Map.Entry<Integer,Object> m: list.entrySet()) {
+            System.out.println("The Key is : "+ m.getKey()+" And the value is : "+ m.getValue());
+        }
     }
-   }
+}
--- src/minAndMaxOfAnArray.java
+++ src/minAndMaxOfAnArray.java
@@ -1,20 +1,18 @@
 public class minAndMaxOfAnArray {
-    static void minMax(int[] arr){
-        int size = arr.length-1 ,min = arr[0], max =arr[0];
-        for(int i = 0; i<size;i++ ){
-            if(min> arr[i]){
-                min = arr[i];
-            }
-            else if(max< arr[i]){
-                max = arr[i];
+    static void minMax(int[] arr) {
+        int size = arr.length , min = arr[0], max = arr[0];
+        for (int j : arr) {
+            if (min > j) {
+                min = j;
+            } else if (max < j) {
+                max = j;
             }
         }
-        System.out.println("Minimum number in the array is "+min+"\nMaximum number in the array is " + max);
+        System.out.println("Minimum number in the array is " + min + "\nMaximum number in the array is " + max);
 
     }
-
     public static void main(String[] args) {
-        int[] arr = {222222,0,12,21,56,-200,556,69955,-1,0,555,4,556,1};
+        int[] arr = {222222,0,12,21,56,-200,556,69955,-1,0,555,4,556,1,-300};
         minMax(arr);
     }
 }
